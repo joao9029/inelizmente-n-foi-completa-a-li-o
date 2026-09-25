8b1fd1c39a1ae777055c865432cef5e1a3cfcd06
joao9029 <joaolucasLicao@gmail.com>
2026-09-25 17:03:00 -0300

    Change primary key from id to cd for multiple tables

--- fazendo-SISTEMA-DE-LAB-main (2)/fazendo-SISTEMA-DE-LAB-main/fazendo-SISTEMA-DE-LAB-main/fazendo-SISTEMA-DE-LAB-main/inc/SQL/Lab.sql
+++ fazendo-SISTEMA-DE-LAB-main (2)/fazendo-SISTEMA-DE-LAB-main/fazendo-SISTEMA-DE-LAB-main/fazendo-SISTEMA-DE-LAB-main/inc/SQL/Lab.sql
@@ -2,34 +2,34 @@ create database laboratorio;
 use laboratorio;
 
 create table labs (
-  id_lab int primary key auto_increment not null,
+  cd_lab int primary key auto_increment not null,
   nr_lab int not null
 );
 
 create table professores (
-  id_professor int primary key auto_increment not null,
+  cd_professor int primary key auto_increment not null,
   nm_professor varchar(100) not null,
   ds_email varchar(100),
   ds_matricula varchar(20)
 );
 
 create table turmas (
-  id_turma int primary key auto_increment not null,
+  cd_turma int primary key auto_increment not null,
   nm_turma varchar(100) not null,
   ds_turma varchar(100)
 );
 
 create table reservas (
-  id_reserva int primary key auto_increment not null,
+  cd_reserva int primary key auto_increment not null,
   id_professor int not null,
   id_turma int not null,
   id_lab int not null,
   hr_inicio datetime not null,
   hr_saida datetime not null,
   ds_reserva enum('livre','reservado') default 'livre' not null,
-  foreign key (id_professor) references professores (id_professor),
-  foreign key (id_turma) references turmas (id_turma),
-  foreign key (id_lab) references labs (id_lab)
+  foreign key (id_professor) references professores (cd_professor),
+  foreign key (id_turma) references turmas (cd_turma),
+  foreign key (id_lab) references labs (cd_lab)
 );
 
 insert into labs (nr_lab) values 
